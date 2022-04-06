bb8080646f2dcb547da8f20736707a762bfb6c10
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2022-04-06 09:40:20 +0530

    Update gcd.cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,9 +1,35 @@
-// Greatest Common Divisior of two numbers 
+// Greatest Common Divisior of two numbers / H.C.F (HIGHEST COMMON FACTOR)
+
 #include <bits/stdc++.h>
 using namespace std;
 
+
+
 int main(){
 int a=18, b=8;
+// using gcd function of C++
 int GCD=__gcd(a,b);
 cout<<"gcd is "<<GCD;
+return 0;
 }
+
+
+
+
+/*
+
+int main(){
+// using Euclid Algotrithm to find gcd
+int a=18 , b=8;
+
+while(b!=0){
+ int rem=a%b;
+ a=b;
+ b=rem;
+
+}
+
+cout<<"GCD is: "<<a;
+return 0; 
+
+}  */
