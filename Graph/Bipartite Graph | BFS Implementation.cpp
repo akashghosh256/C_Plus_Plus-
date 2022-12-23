37f9959f424ab572aafd5f8b322e2f492aa985de
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2022-12-23 11:51:25 +0530

    Create Bipartite Graph | BFS Implementation.cpp

---
+++ Graph/Bipartite Graph | BFS Implementation.cpp
@@ -0,0 +1,148 @@
+                                                             C++
+                                     #Striver                          
+                                                               
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    // colors a component 
+    private: 
+    bool check(int start, int V, vector<int>adj[], int color[]) {
+        queue<int> q;
+	    q.push(start); 
+	    color[start] = 0; 
+	    while(!q.empty()) {
+	        int node = q.front();
+	        q.pop(); 
+	        
+	        for(auto it : adj[node]) {
+	            // if the adjacent node is yet not colored
+	            // you will give the opposite color of the node 
+	            if(color[it] == -1) {
+	                
+	                color[it] = !color[node]; 
+	                q.push(it); 
+	            }
+	            // is the adjacent guy having the same color 
+	            // someone did color it on some other path 
+	            else if(color[it] == color[node]) {
+	                return false; 
+	            }
+	        }
+	    }
+	    return true; 
+    }
+public:
+	bool isBipartite(int V, vector<int>adj[]){
+	    int color[V]; 
+	    for(int i = 0;i<V;i++) color[i] = -1; 
+	    
+	    for(int i = 0;i<V;i++) {
+	        // if not coloured
+	        if(color[i] == -1) {
+	            if(check(i, V, adj, color) == false) {
+	                return false; 
+	            }
+	        }
+	    }
+	    return true; 
+	}
+
+};
+
+void addEdge(vector <int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+int main(){
+	
+	// V = 4, E = 4
+	vector<int>adj[4];
+	
+	addEdge(adj, 0, 2);
+   	addEdge(adj, 0, 3);
+    	addEdge(adj, 2, 3);
+    	addEdge(adj, 3, 1);
+
+	Solution obj;
+	bool ans = obj.isBipartite(4, adj);    
+	if(ans)cout << "1\n";
+	else cout << "0\n";  
+	
+	return 0;
+}
+
+
+--------------------------------------------------------------------------------------------------------------------
+                                                          Java
+
+import java.util.*;
+
+class Solution
+{
+    private boolean check(int start, int V, 
+    ArrayList<ArrayList<Integer>>adj, int color[]) {
+        Queue<Integer> q = new LinkedList<Integer>();
+	    q.add(start); 
+	    color[start] = 0; 
+	    while(!q.isEmpty()) {
+	        int node = q.peek();
+	        q.remove(); 
+	        
+	        for(int it : adj.get(node)) {
+	            // if the adjacent node is yet not colored
+	            // you will give the opposite color of the node 
+	            if(color[it] == -1) {
+	                
+	                color[it] = 1 - color[node]; 
+	                q.add(it); 
+	            }
+	            // is the adjacent guy having the same color 
+	            // someone did color it on some other path 
+	            else if(color[it] == color[node]) {
+	                return false; 
+	            }
+	        }
+	    }
+	    return true; 
+    }
+
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        int color[] = new int[V]; 
+	    for(int i = 0;i<V;i++) color[i] = -1; 
+	    
+	    for(int i = 0;i<V;i++) {
+	        if(color[i] == -1) {
+	            if(check(i, V, adj, color) == false) {
+	                return false; 
+	            }
+	        }
+	    }
+	    return true; 
+    }
+    public static void main(String[] args)
+    {
+        // V = 4, E = 4
+        ArrayList < ArrayList < Integer >> adj = new ArrayList < > ();
+        for (int i = 0; i < 4; i++) {
+            adj.add(new ArrayList < > ());
+        }
+        adj.get(0).add(2);
+        adj.get(2).add(0);
+        adj.get(0).add(3);
+        adj.get(3).add(0);
+        adj.get(1).add(3);
+        adj.get(3).add(1);
+        adj.get(2).add(3);
+        adj.get(3).add(2);
+
+        Solution obj = new Solution();
+        boolean ans = obj.isBipartite(4, adj);
+        if(ans)
+            System.out.println("1");
+        else System.out.println("0");
+    }
+
+}
