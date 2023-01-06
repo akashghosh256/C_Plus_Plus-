c01c54ddcd0b80db12c275b7758bd794e1c5a50c
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-01-07 01:43:27 +0530

    Create Priority Queue.cpp

---
+++ Priority Queue.cpp
@@ -0,0 +1,11 @@
+Dijkstras Algorithm
+{dis,vertex}
+priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+
+
+{diff, {row,col}}
+ priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>, greater<pair<int,pair<int,int>>>> pq;
+
+
+
