afd9d6342cf1f9247db2f99a927fb0f1b165b070
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-01-19 17:27:19 +0530

    Update BFS(QUEUE).cpp

--- Graph/BFS(QUEUE).cpp
+++ Graph/BFS(QUEUE).cpp
@@ -1,137 +1,67 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+
 using namespace std;
-const char nl ='\n';
-/* Function to add an Edge */
-void Edge(vector<int> adj[], int v1, int v2)
-{
-    /* considering undirected graph, hence adding both forward and backward edge */
-    adj[v1].push_back(v2);
-    adj[v2].push_back(v1);
-    
-}
+class Solution {
+  public:
+    vector < int > bfsOfGraph(int V, vector < int > adj[]) {
+      vector < int > bfs;
+      vector < int > vis(V, 0);
+      queue < int > q;
+      q.push(0);
+      vis[0] = 1;
+      while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
 
-void print_Edge(vector<int> adj[])
-{  //Printing Array of Vectors
-    for(int i=0; i<7; i++)
-    {
-        cout<<nl<<"Adjacency List of vertex "<<i<<": ";
-        for(int j=0; j<adj[i].size(); j++)
-        {
-            cout<<adj[i][j]<<", ";
+        for (auto it: adj[node]) {
+          if (!vis[it]) {
+            q.push(it);
+            vis[it] = 1;
+          }
         }
-        cout<<nl;
+      }
+
+      return bfs;
     }
+};
+
+void addEdge(vector < int > adj[], int u, int v) {
+  adj[u].push_back(v);
+  adj[v].push_back(u);
 }
 
-/* Simple BFS Function */
-void BFS(int v1, vector<int> adj[], vector<bool> &Visited)
-{   cout<<"BFS called for: "<<v1<<endl;
-    /* Initialize a queue for keeping track of upcoming vertices */
-    list<int> queue_of_vertices;    // or use Queue<int> q;
- 
-    /* Mark the current vertex visited and pushed to the queue*/
-    Visited[v1] = true;
-    queue_of_vertices.push_back(v1);
- 
-    /* while the queue is not empty, keep on traversing */
-    while(!queue_of_vertices.empty())
-    {
-        /* remove the front vertex and print it*/
-        v1 = queue_of_vertices.front();
-        cout<<nl <<"OutPut :"<< v1 << nl;
-        queue_of_vertices.pop_front();
- 
-        /* Add all the unvisited adjacent vertices of v1 to the queue */
-        for (int iter = 0; iter != adj[v1].size(); iter++)
-        {
-            if (!Visited[adj[v1][iter]])
-            {
-                Visited[adj[v1][iter]] = true;
-                cout<<" iter= "<<iter<<"  calling for: "<<adj[v1][iter]<<endl;
-                queue_of_vertices.push_back(adj[v1][iter]);
-            }
-        }
-    cout<<"cureent queue: "<<endl;
-    for (auto itr : queue_of_vertices)
-        cout<<itr<<" ";
-    cout<<endl;
-
-    }
+void printAns(vector < int > & ans) {
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] << " ";
+  }
 }
- 
-/* Driver Function */
-int main()
-{
-    int number_of_vertices = 7;
-    vector<int> adj[number_of_vertices];
- 
-    /* Adding edges */
-    Edge(adj, 0, 1);
-    Edge(adj, 0, 2);
-    Edge(adj, 1, 2);
-    Edge(adj, 1, 3);
-    Edge(adj, 2, 3);
-    Edge(adj, 4, 5);
-    Edge(adj, 4, 6);
-    print_Edge(adj);
-   
-    vector<bool> Visited(number_of_vertices, false);
-    for (int u=0; u<number_of_vertices; u++)
-        if (Visited[u] == false){
-            cout<<"from main calling for: "<<u<<endl;
-            BFS(u, adj, Visited);
-        }
-    return 0;
+int main() {
+  vector < int > adj[5];
+
+  addEdge(adj, 0, 1);
+  addEdge(adj, 0, 2);
+  addEdge(adj, 0, 3);
+  addEdge(adj, 2, 4);
+
+  Solution obj;
+  vector < int > ans = obj.bfsOfGraph(5, adj);
+  printAns(ans);
+  cout << endl;
+
+  return 0;
 }
 
+Output:
 
-OUTPUT:
-Adjacency List of vertex 0: 1, 2, 
+0 1 2 3 4
 
-Adjacency List of vertex 1: 0, 2, 3,
+Time Complexity : O(N+E)
 
-Adjacency List of vertex 2: 0, 1, 3,
+N = Nodes , E = travelling through adjacent nodes
 
-Adjacency List of vertex 3: 1, 2,
+Space Complexity : O(N+E) + O(N) + O(N) 
 
-Adjacency List of vertex 4: 5, 6,
+Space for adjacency list, visited array, queue data structure
 
-Adjacency List of vertex 5: 4,
 
-Adjacency List of vertex 6: 4,
-from main calling for: 0
-BFS called for: 0
-
-OutPut :0
- iter= 0  calling for: 1
- iter= 1  calling for: 2
-cureent queue:
-1 2
-
-OutPut :1
- iter= 2  calling for: 3
-cureent queue:
-2 3
-
-OutPut :2
-cureent queue:
-3
-
-OutPut :3
-cureent queue:
-
-from main calling for: 4
-BFS called for: 4
-
-OutPut :4
- iter= 0  calling for: 5
- iter= 1  calling for: 6
-cureent queue:
-5 6
-
-OutPut :5
-cureent queue:
-6
-
-OutPut :6
-cureent queue:
