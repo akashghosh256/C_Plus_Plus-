93bafb7584b7edbbebb2c3b0f75c5721aa2fd518
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-01-19 17:28:28 +0530

    Update DFS(STACK).cpp

--- Graph/DFS(STACK).cpp
+++ Graph/DFS(STACK).cpp
@@ -1,96 +1,57 @@
-// C++ program to print DFS
-// traversal for a given
-// graph
 #include <bits/stdc++.h>
 using namespace std;
-const char nl = '\n';
-
-class Graph {
-
-    // A function used by DFS
-    void DFSUtil(int v);
-
-public:
-    map<int, bool> visited;
-    map<int, list<int> > adj;
-    // function to add an edge to graph
-    void addEdge(int v, int w);
-    void print_map();
-    // prints DFS traversal of the complete graph
-    void DFS();
-};
-
-void Graph::addEdge(int v, int w)
-{
-    adj[v].push_back(w); // Add w to v’s list.
-}
-
-
-void print_map(map<int, list<int> > adj){
-    cout << "Map: " << endl;
-    for (auto itr : adj){
-        cout << itr.first << ":\t";
-        for (auto innerItr : itr.second)
-            cout << innerItr << " ";
-        cout << endl;
-    }
-}
-void Graph::DFSUtil(int v)
-{
-    // Mark the current node as visited and print it
-    visited[v] = true;
-    cout<<nl<<"print " << v << " ";
-
-
-    // Recur for all the vertices adjacent to this vertex
-    list<int>::iterator i;  //auto i
-    for (i = adj[v].begin(); i != adj[v].end(); ++i){
-       
-        cout<<nl<<"checking: "<<endl;
-        if (!visited[*i]){
-            cout<<"call again DFSUtil: "<<visited[*i]<<endl;
-            DFSUtil(*i);
+class Solution {
+    void dfs(int node, vector<int> &vis, vector<int> adj[], vector<int> &storeDfs) {
+        storeDfs.push_back(node); 
+        vis[node] = 1; 
+        for(auto it : adj[node]) {
+            if(!vis[it]) {
+                dfs(it, vis, adj, storeDfs); 
+            }
         }
-        else cout<<"skip/over"<<nl;
     }
-}
-
-// The function to do DFS traversal. It uses recursive
-// DFSUtil()
-void Graph::DFS()
+public:
+	vector<int>dfsOfGraph(int V, vector<int> adj[]){
+	    vector<int> storeDfs; 
+	    vector<int> vis(V+1, 0); 
+      for(int i = 1;i<=V;i++) {
+        if(!vis[i]) dfs(i, vis, adj, storeDfs); 
+      }
+	    return storeDfs; 
+	}
+};
+ 
+void addEdge(vector<int> adj[],int u,int v)
 {
-    // Call the recursive helper function to print DFS
-    // traversal starting from all vertices one by one
-    for (auto i : adj){
-        if (visited[i.first] == false){
-            cout<< "dfs1 calll: "<<i.first<<endl;
-            DFSUtil(i.first);
-        }}
+    adj[u].push_back(v);
+    adj[v].push_back(u);
 }
-
-// Driver's Code
 int main()
 {
-    // Create a graph given in the above diagram
-    Graph g;
-    g.addEdge(0, 1);
-    g.addEdge(0, 9);
-    g.addEdge(1, 2);
-    g.addEdge(2, 0);
-    g.addEdge(2, 3);
-    g.addEdge(9, 3);
-
-
-
-
+    vector<int> adj[6];
     
-    print_map(g.adj);
-    // Function call
-    
-    cout << "Following is Depth First Traversal \n";
-    g.DFS();
-    
-
+    addEdge(adj,1,2);
+    addEdge(adj,1,3);
+    addEdge(adj,1,4);
+    addEdge(adj,1,5);
+    addEdge(adj,2,4);
+    addEdge(adj,2,1);
+    addEdge(adj,3,1);
+    addEdge(adj,4,1);
+    addEdge(adj,4,2);
+    addEdge(adj,5,1);
+    Solution obj;
+    vector<int> df;
+    df= obj.dfsOfGraph(5, adj);
+    for(auto it: df)
+    cout<<it<<" ";
+ 
     return 0;
 }
-// improved by Vishnudev C
+Output:
+
+1 2 4 3 5
+
+Time complexity: O(N+E), Where N is the time taken for visiting N nodes and E is for travelling through adjacent nodes.
+
+Space Complexity:O(N+E)+O(N)+O(N) , Space for adjacency list, Array,Auxiliary space.
