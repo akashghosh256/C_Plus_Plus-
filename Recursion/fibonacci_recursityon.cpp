9a4d2810b19e245baef818657b0ba903e65c20ec
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-01-27 17:44:57 +0530

    Update fibonacci_recursityon.cpp

--- Recursion/fibonacci_recursityon.cpp
+++ Recursion/fibonacci_recursityon.cpp
@@ -1,3 +1,10 @@
+The time complexity of the Fibonacci series algorithm that uses recursion is O(2^n).
+
+The reason for this is that for each call of the function, two new calls are made, 
+one with the input decremented by 1 and the other with the input decremented by 2. 
+This means that the number of recursive calls made is roughly equivalent to the Fibonacci sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+
+ Yes, the space complexity of the Fibonacci series algorithm that uses recursion is also O(2^n).
 
 #include<bits/stdc++.h>
 using namespace std;
