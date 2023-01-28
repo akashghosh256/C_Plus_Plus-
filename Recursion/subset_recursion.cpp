b80a0144c282a3d84cd83ceb035e1f014f9ab093
Akash Ghosh <93257481+akashghosh256@users.noreply.github.com>
2023-01-28 22:43:50 +0530

    Update subset_recursion.cpp

--- Recursion/subset_recursion.cpp
+++ Recursion/subset_recursion.cpp
@@ -1,34 +1,95 @@
 #include "bits/stdc++.h"
 using namespace std;
 
+
 // printing subset using recursion
 // can be also done using bitmask
 // bitmask done check it---------------------------------------
-void subset(vector<int>input,vector<int>output,int index){
+// EXAMPLE OF SUBSET : {1,2,3}  SUBSET : {},{1},{2},{3},{1,2},{1,3},{2,3},{1,2,3}
 
-if(index>=input.size()){
-    for(auto i:output){
-        cout<<i<<" ";
+// STRIVER METHOD
+void subset(int ind, vector<int> &ds, int arr[], int n){
+    if(ind == n){
+        for(auto it : ds){
+            cout << it << " ";
+        }
+
+        if(ds.size() == 0){
+            cout<<"{}";  // to show empty set
+        }
+
+        cout << endl;
+        return;
+
     }
-
-cout<<endl;
-return;
-}
-
-
-subset(input,output,index+1);
-output.push_back(input[index]);
-subset(input,output,index+1);
+    subset(ind+1, ds, arr, n);
+    ds.push_back(arr[ind]);   // PICK
+    subset(ind+1, ds, arr, n);
+    ds.pop_back();   // NOT PICK
 }
 
 int main(){
-vector<int> input={1,2,3};
-vector<int> output;
-subset(input,output,0);
-return 0;
+    int arr[] = {3,1,2};
+    int n = 3;
+    vector<int> ds;  // data structure to store the subset
+    subset(0, ds, arr, n);
+    return 0;
+
 }
+ // TC - O(2^n)  FOR EACH ELEMENT WE HAVE TWO CHOICES
+// SC - O(n)  BECAUSE  DEPTH OF RECURSION STACK WILL BE N
+// OUTPUT:
+// {}
+// 2
+// 1
+// 1 2
+// 3
+// 3 2
+// 3 1
+// 3 1 2
+
+//--------------------------------------------------------------------------
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// OLD METHOD
+
+// void subset(vector<int>input,vector<int>output,int index){
+
+// if(index>=input.size()){
+// for(auto i:output){
+// cout<<i<<" ";
+// }
+
+// cout<<endl;
+// return;
+// }
+
+
+// subset(input,output,index+1);
+// output.push_back(input[index]);
+// subset(input,output,index+1);
+// }
+
+// int main(){
+// vector<int> input={1,2,3};
+// vector<int> output;
+// subset(input,output,0);
+// return 0;
+// }
+
+/*
 OUTPUT
 3 
 2 
@@ -37,3 +98,4 @@ OUTPUT
 1 3 
 1 2 
 1 2 3 
+*/
